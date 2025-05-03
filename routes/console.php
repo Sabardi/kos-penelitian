31761114aa84c2ab8e2c99d8913043ea7b43bed1
Sabardi <Sabardibahari@gmail.com>
2025-05-04 05:53:29 +0700

    Refactor console commands to improve structure and scheduling

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Console\Commands\GenerateSimilarity;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\DB;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('similarity:generate', function () {
+    $this->call(GenerateSimilarity::class);
+})->purpose('Delete recent users')->everySecond();
